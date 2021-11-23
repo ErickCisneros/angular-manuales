5819472fb5193b6c20bebf387eca6639c80e84cc
Erick Cisneros <erickxz10@gmail.com>
2021-11-22 19:40:35 -0500

    Se ha creado la pagina principal

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const materials: any[] = [
-
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
 ]
 
 @NgModule({
